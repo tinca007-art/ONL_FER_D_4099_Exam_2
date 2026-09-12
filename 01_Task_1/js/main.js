a002080d597f1844d599cd13bd745215c15ec597
“Kristina <tinca007@seznam.cz>
2026-09-12 08:19:52 +0200

    task 1

--- 01_Task_1/js/main.js
+++ 01_Task_1/js/main.js
@@ -1,5 +1,16 @@
 const data = [1, 2, 3, 4, 5];
 
+function without(data, ...num) {
+    return data.filter(item => !num.includes(item));
+}
+
+console.log(without(data, 1, 2)); // [3, 4, 5];
+console.log(without(data, 5, 3)); // [1, 2, 4];
+console.log(without(data, 1, 2, 3, 4)); // [5];
+console.log(without(data)); // [1, 2, 3, 4, 5];
+console.log(without(data, 1, 2, 3, 4, 5)); // [];
+
+
 /**
  * Write your code below!
  */
