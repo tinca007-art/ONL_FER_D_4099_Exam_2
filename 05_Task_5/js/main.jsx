6ed9bd0ba5adbbaa73a7596ec83a2c51a1bcdf10
“Kristina <tinca007@seznam.cz>
2026-09-12 09:00:29 +0200

    Task 5

--- 05_Task_5/js/main.jsx
+++ 05_Task_5/js/main.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-/**
- * Do not modify the code below!
- */
+
+function UserDetails({ name, surname, address, postcode, city }) {
+    return (
+        <div className="card">
+            <div>
+                <h5>{name} {surname}</h5>
+                <div>{address}</div>
+                <div>{postcode} {city}</div>
+            </div>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <UserDetails
+            name="Jan"
+            surname="Novák"
+            address="Hlavní 123"
+            postcode="11000"
+            city="Praha"
+        />
+    );
+}
+
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
